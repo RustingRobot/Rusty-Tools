53313f4ced74ddac51dd8348fd8753a11c1be53e
Rusting Robot <rusting.robot.scratch@gmail.com>
2020-03-12 20:29:41 +0100

    new Icons

---
+++ injectRusty.js
@@ -0,0 +1,7 @@
+// all added features by RustingRobot are here
+console.log("Rusty features loaded");
+
+while(true){
+  console.log("shit");
+  document.getElementsByClassName("label_input-label_3KjCa").style.color = "blue";
+}
--- injectSimple.js
+++ injectSimple.js
@@ -1,7 +1,10 @@
 const s = document.createElement('script');
+const r = document.createElement('script');
 // TODO: add "script.js" to web_accessible_resources in manifest.json
 s.src = chrome.runtime.getURL('inject3.js');
+r.src = chrome.runtime.getURL('injectRusty.js');
 s.onload = function() {
     this.remove();
 };
 (document.head || document.documentElement).appendChild(s);
+(document.head || document.documentElement).appendChild(r);
