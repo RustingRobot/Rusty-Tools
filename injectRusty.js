81dc3107817e78bc9d3accbc35ff5697713766a6
Rusting Robot <rusting.robot.scratch@gmail.com>
2020-03-14 22:34:16 +0100

    starting stone for onion skining

--- injectRusty.js
+++ injectRusty.js
@@ -1,7 +1,56 @@
 // all added features by RustingRobot are here
 console.log("Rusty features loaded");
 
-while(true){
-  console.log("testing");
-  document.getElementsByClassName("label_input-label_3KjCa").style.color = "blue";
+let costumesBtn = document.getElementById('react-tabs-2');
+var oSBtn;
+
+costumesBtn.addEventListener("click", function() {
+    setTimeout(CostumesClick,0);
+}, false);
+
+function CostumesClick(){
+  let convertBtn = document.getElementsByClassName('button_button_u6SE2 paint-editor_bitmap-button_keW7B');
+  convertBtn[0].insertAdjacentHTML('afterend', `
+    <div class= "OnionSkin-container">
+      <label class="onionSkinBtn" value="OFF">
+        <input type="checkbox" id="osBtn">
+        <span class="btnImg"></span>
+      </label>
+      <input class="osAlphaInp" id="osAlphaInp" type = "text"></input>
+      </dev>
+  `);
+
+  oSBtn  = document.getElementById("osBtn");
+  oSBtn.value = "OFF";
+  oSBtn.addEventListener("change", function() {
+      OSClick();
+  }, false);
 }
+
+function OSClick(){
+  if (oSBtn.value == "OFF") {
+    oSBtn.value = "ON";
+  } else {
+    oSBtn.value = "OFF";
+  }
+  console.log(oSBtn.value);
+}
+
+/*
+
+
+
+<div>
+  <span class="button_button_u6SE2 tool-select-base_mod-tool-select_1iD-h" role="button" title="OnionSkin">
+    <button type="checkbox" class="onionSkinBtn" value="ON" onclick="OSClick(this)"></button>
+  </span>
+</div>
+
+<span class="button_button_u6SE2 tool-select-base_mod-tool-select_1iD-h" role="button" title="OnionSkin">
+  <label class="onionSkinBtn">
+    <input type="checkbox">
+    <span class="btnImg"></span>
+  </label>
+</span>
+
+*/
