1813d014c02f85c1d0a3065bd1c094e723394ad9
Rusting Robot <rusting.robot.scratch@gmail.com>
2020-03-23 15:28:23 +0100

    activation bug fixing + can now display costumes in canvas

--- injectRusty.js
+++ injectRusty.js
@@ -1,56 +1,63 @@
 // all added features by RustingRobot are here
 console.log("Rusty features loaded");
 
-let costumesBtn = document.getElementById('react-tabs-2');
+//variables for all of this script
 var oSBtn;
+var osValue=false;
+var costumeImages;
 
-costumesBtn.addEventListener("click", function() {
-    setTimeout(CostumesClick,0);
-}, false);
+if (document.URL.includes('editor')) {asignElements();}
+else{waitForEditor();}
 
-function CostumesClick(){
-  let convertBtn = document.getElementsByClassName('button_button_u6SE2 paint-editor_bitmap-button_keW7B');
-  convertBtn[0].insertAdjacentHTML('afterend', `
-    <div class= "OnionSkin-container">
-      <label class="onionSkinBtn" value="OFF">
-        <input type="checkbox" id="osBtn">
-        <span class="btnImg"></span>
-      </label>
-      <input class="osAlphaInp" id="osAlphaInp" type = "text"></input>
-      </dev>
-  `);
-
-  oSBtn  = document.getElementById("osBtn");
-  oSBtn.value = "OFF";
-  oSBtn.addEventListener("change", function() {
-      OSClick();
+function asignElements(){//set up everything in the editor (addEventListeners etc.)
+  var costumesBtn = document.getElementById('react-tabs-2');
+  costumesBtn.addEventListener("click", function() {
+    waitForElement(); //wait for the costumes tab to load (more sprites more lag)
   }, false);
 }
-
-function OSClick(){
-  if (oSBtn.value == "OFF") {
-    oSBtn.value = "ON";
-  } else {
-    oSBtn.value = "OFF";
-  }
-  console.log(oSBtn.value);
-}
-
-/*
-
-
-
-<div>
-  <span class="button_button_u6SE2 tool-select-base_mod-tool-select_1iD-h" role="button" title="OnionSkin">
-    <button type="checkbox" class="onionSkinBtn" value="ON" onclick="OSClick(this)"></button>
-  </span>
-</div>
-
-<span class="button_button_u6SE2 tool-select-base_mod-tool-select_1iD-h" role="button" title="OnionSkin">
+function waitForEditor(){//loops until the url contains "editor"
+  if(document.URL.includes('editor')){asignElements(); }
+  else{window.setTimeout(waitForEditor,100);}}
+function waitForElement(){  //loops until the bitmap button exists
+  if(!!document.getElementsByClassName('button_button_u6SE2 paint-editor_bitmap-button_keW7B')[0]){CostumesClick(); }
+  else{window.setTimeout(waitForElement,100);}}
+function CostumesClick(){ //append the onion skin button (and other text fields)
+  if(!!document.getElementById("osBtn")){return;} //check if this function already got executed
+  //Onion Skin buttons and text fields:
+  let convertBtn = document.getElementsByClassName('button_button_u6SE2 paint-editor_bitmap-button_keW7B');
+  convertBtn[0].insertAdjacentHTML('afterend', `
   <label class="onionSkinBtn">
-    <input type="checkbox">
+    <input type="checkbox" id="osBtn">
     <span class="btnImg"></span>
   </label>
-</span>
+    <div class= "OnionSkin-container">
+      <span class="alphaInputSpan">
+        <input class="osAlphaInp" id="osAlphaInp" type = "text" placeholder="opacity"></input>
+        <span class="label_input-label_1s1ft">%</span>
+      </span>
+      <span>
+        <input class="osAlphaInp" id="osFrameNr" type = "text" placeholder="# frame"></input>
+      </span>
+    </div>
+  `);
+  var osBtn  = document.getElementById("osBtn");
+  if(osValue){osBtn.checked=true;}
+  else{osBtn.checked=false;}
+  osBtn.addEventListener("change", function() {
+      OSClick();
+  }, false);
+  //getty all images
+  costumeImages = document.getElementsByClassName("sprite-selector-item_sprite-image_2QWuK");
 
-*/
+}
+
+function OSClick(){//set value for the onion skin button
+  osValue = !osValue;
+  if(osValue){
+    let horizontalScroll= document.getElementsByClassName("scrollable-canvas_horizontal-scrollbar_2w6OD")[0];
+    let verticalScroll= document.getElementsByClassName("scrollable-canvas_vertical-scrollbar_lbD_c")[0];
+    let canvas = document.getElementsByClassName("paper-canvas_paper-canvas_1y588");
+    let ctx = canvas[0].getContext("2d");
+    ctx.drawImage(costumeImages[1],10,10,costumeImages[1].width,costumeImages[1].height);
+  }
+}
