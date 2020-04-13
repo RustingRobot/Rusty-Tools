c05b825e7ef2923e262f7ce53597299a3b69383b
Rusting Robot <rusting.robot.scratch@gmail.com>
2020-04-13 16:06:42 +0200

    redo draw loop

--- injectRusty.js
+++ injectRusty.js
@@ -69,86 +69,8 @@ function OSClick(){//set value for the onion skin button
   }
 
   function DrawLoop(){
-    let frameNr = document.getElementById("osFrameNr");
-    let opacity = document.getElementById("osAlphaInp");
-    try{
-      zoomLvl = 140 / parseInt(horizontalScroll.style.width);//170
-      xZoom =100/((100 + costumeImages[parseInt(frameNr.value)].width) / zoomLvl);
-      yZoom =100/((100 + costumeImages[parseInt(frameNr.value)].height) / zoomLvl);
-      midCanvasX = (canvas.width / 2) -(costumeImages[parseInt(frameNr.value)].width / 2 *zoomLvl);
-      midCanvasY = (canvas.height / 2) - (costumeImages[parseInt(frameNr.value)].height / 2 *zoomLvl);
-      dCanvas.clearRect(0, 0, canvas.width, canvas.height);
-      canvas.style["opacity"] = parseInt(opacity.value) / 100;
-      //dCanvas.drawImage(costumeImages[1],midCanvasX,midCanvasY,costumeImages[1].width * zoomLvl,costumeImages[1].height * zoomLvl);
-      dCanvas.strokeStyle = "red";
-      dCanvas.lineWidth = "6";
-      dCanvas.drawImage(costumeImages[parseInt(frameNr.value)],midCanvasX,midCanvasY,costumeImages[parseInt(frameNr.value)].width * zoomLvl,costumeImages[parseInt(frameNr.value)].height * zoomLvl);
-    }
-    catch{
-      dCanvas.clearRect(0, 0, canvas.width, canvas.height);
-      //dCanvas.drawImage(costumeImages[0],midCanvasX,midCanvasY,costumeImages[1].width * zoomLvl,costumeImages[1].height * zoomLvl);
-    }
+    
     if(osValue){window.setTimeout(DrawLoop,0);}
     else{return;}
   }
 }
-
-/*function groupVariables(){
-  let flyout = document.getElementsByClassName("blocklyFlyout")[0];
-  let ws = flyout.childNodes[1].childNodes[0];
-  var blocks = ws.childNodes;
-  if(blocks.length < 228){window.setTimeout(groupVariables,1000); return;}
-  var makeVarBtn = document.getElementsByClassName("blocklyFlyoutButton")[0];
-  var minY = yOf(makeVarBtn);
-  var maxY = 0;
-  var vars = [];
-  for (var i = 0; i < blocks.length; i++) {
-    if(!!blocks[i].getAttribute('transform')){
-      if(maxY == 0){
-        if(blocks[i].getAttribute('data-id') == "data_setvariableto"){
-          maxY = parseInt(yOf(blocks[i]));
-          i=-1; continue;
-        }
-      }else if (yOf(blocks[i]) > 8108 && yOf(blocks[i]) < maxY){
-        vars.push(blocks[i]);
-      }
-    }
-  }
-  let varLable = document.getElementsByClassName("blocklyFlyoutLabelText")[7];
-  varLable.innerHTML += " (" + vars.length / 2 + ")";
-  varLable.classList.add('newLabel');
-  varLable.setAttribute("x",50 + 8 * String(vars.length / 2).length);
-  insertCollapsables(makeVarBtn, vars);
-
-  function yOf(element){
-    let tempY = element.getAttribute('transform');
-    tempY = tempY.split(",")[1];
-    return tempY.substring(0,tempY.length - 1);
-  } //returns the X of a translate()
-}*/
-/*
-function insertCollapsables(makeVarBtn, vars){
-  var s = new XMLSerializer();
-  makeVarBtn.insertAdjacentHTML('afterend', `
-  <g class="collapsible" transform="translate(12,8160)" display="block" id = "collapsible">
-    <rect class="collapsableBG" rx="4" ry="4" width="194.4499969482422" height="40"></rect>
-    <text class="blocklyText" x="97.2249984741211" y="20" text-anchor="middle" dominant-baseline="central" dy="0">test</text>
-  </g>`);
-
-  vars[3].classList.add('content');
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
-
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-      this.classList.toggleclassList("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
-    });
-  }
-}
-*/
